8522cb7aca5c5164fc42b307d335bf3ad650ce08
LeoAOliveira <leonardo.aoliveira@icloud.com>
2020-04-18 17:34:12 -0300

    Vision implementado com integração do modelo customizado

---
+++ Leo Useless App/Leo Useless App/Controller/StatusViewController.swift
@@ -0,0 +1,25 @@
+//
+//  StatusViewController.swift
+//  Leo Useless App
+//
+//  Created by Leonardo Oliveira on 18/04/20.
+//  Copyright © 2020 Leonardo Oliveira. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StatusViewController: UIViewController {
+
+
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
